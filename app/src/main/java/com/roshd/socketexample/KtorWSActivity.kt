ee57ca2128f887ef05ea625714abfab33c60a49e
mohammadT77 <t.mohammadamin@gmail.com>
2021-08-23 19:52:02 +0430

    Documenting KtorWSActivity.kt

--- app/src/main/java/com/roshd/socketexample/KtorWSActivity.kt
+++ app/src/main/java/com/roshd/socketexample/KtorWSActivity.kt
@@ -19,9 +19,9 @@ import kotlinx.coroutines.*
 class KtorWSActivity : AppCompatActivity() {
 
     companion object {
-        const val HOST = "192.168.1.6"
+        const val HOST = "192.168.1.6"  // Note: "localhost" or "128.0.0.1" is invalid according to Android OS policies
         const val PORT = 8000
-        const val PATH = "/ws/chat/akbar/"
+        const val PATH = "/ws/chat/a_chat_room/"  // Chat room: a_chat_room
     }
 
     lateinit var button: Button
@@ -35,26 +35,32 @@ class KtorWSActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Binding UI views
         button = findViewById<View>(R.id.sendBtn) as Button
         textView = findViewById<View>(R.id.receivedTextView) as TextView
         editText = findViewById<View>(R.id.sendEditText) as EditText
 
+        // Instancing Ktor Http client
         val client = HttpClient {
+            // Installing WebSockets feature into the http client
             install(WebSockets)
         }
 
-
-//        button.setOnClickListener {
+        // Start a Coroutine to Receiving websocket messages:
         CoroutineScope(Dispatchers.IO).launch {
+            // Start a websocket session wrapped by the coroutine
             client.webSocket(
                 method = HttpMethod.Get,
                 host = HOST,
                 port = PORT, path = PATH
             ) {
                 while (true) {
-                    val frame = incoming.receive()
+                    val frame = incoming.receive()  // Waiting for incoming messages
                     if (frame is Frame.Text) {
+                        // Parse the received json message to a Message instance.
                         val message: Message? = Klaxon().parse<Message>(frame.readText())
+
+                        // Post the UI view, to update textview with the received message.
                         textView.post {
                             textView.text = textView.text.toString() + "\n" + message!!.text
                         }
@@ -65,14 +71,20 @@ class KtorWSActivity : AppCompatActivity() {
 
         }
 
+        // Setting up send button listener:
         button.setOnClickListener {
             runBlocking {
+                // Start a websocket session again! TODO: requires revision
                 client.webSocket(
                     method = HttpMethod.Get,
                     host = HOST,
                     port = PORT, path = PATH
                 ) {
-                    send(Klaxon().toJsonString(Message(editText.text.toString())))
+                    // Convert editText message into a Message Instance.
+                    val msg_to_send = Klaxon().toJsonString(Message(editText.text.toString()))
+
+                    // Send the message toward the server
+                    send(msg_to_send)
                 }
             }
         }
